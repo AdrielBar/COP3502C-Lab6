cf7736ccded5e02876790cfc572969802dd8ea7a
Ernesto <motovation0313@gmail.com>
2023-03-22 23:55:21 -0400

    done

--- password_encoder.py
+++ password_encoder.py
@@ -21,6 +21,22 @@ def print_menu():
           f"2. Decode\n"
           f"3. Quit\n")
 
+def decode(encoded_passwd):
+    # Check if encoded password is exactly 8 digits long
+    if len(encoded_passwd) != 8:
+        raise ValueError("Encoded password must be exactly 8 digits long")
+    # Decode each digit by shifting it down by 3
+    decoded_digits = []
+    for digit in encoded_passwd:
+        # Convert digit from string to integer
+        digit_int = int(digit)
+        # Shift digit down by 3, wrapping around to 9 after 0
+        decoded_digit_int = (digit_int - 3) % 10
+        # Convert decoded digit back to string and append to list
+        decoded_digits.append(str(decoded_digit_int))
+    # Convert list of decoded digits to string
+    decoded_password = ''.join(decoded_digits)
+    return decoded_password
 
 if __name__ == "__main__":
     encoded_passwd = ''
@@ -31,6 +47,8 @@ if __name__ == "__main__":
             user_passwd = input("Please enter your password to encode: ")
             encoded_passwd = encode(user_passwd)
         elif option == 2:
-            pass
+            decoded_passwd = decode(encoded_passwd)
+            # Print the original and encoded password
+            print(f"The encoded password is {encoded_passwd}, and the original password is {decoded_passwd}.")
         elif option == 3:
             break
